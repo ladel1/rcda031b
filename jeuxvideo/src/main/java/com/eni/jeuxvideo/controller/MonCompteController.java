f3cb1737d8a44a249521919ee4c5cdefb54ff68f
Adel LATIBI <alatibi@campus-eni.fr>
2024-01-26 16:25:38 +0100

    auth

--- jeuxvideo/src/main/java/com/eni/jeuxvideo/controller/MonCompteController.java
+++ jeuxvideo/src/main/java/com/eni/jeuxvideo/controller/MonCompteController.java
@@ -1,5 +1,15 @@
 package com.eni.jeuxvideo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MonCompteController {
 
+	@GetMapping("/mon-compte")
+	public String monCompte() {
+		return "backoffice/mon-compte";
+	}
+	
+	
 }
